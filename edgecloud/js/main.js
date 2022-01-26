2698c6302abb6e30c23f36bc58a0c589cfb58b22
Vinnicius MOntanha <viniciusmassis01@hotmail.com>
2022-01-26 11:51:03 -0300

    final commit

---
+++ edgecloud/js/main.js
@@ -0,0 +1,5 @@
+var input = document.getElementById('#inputPassword');
+var img = document.getElementById('#togglePassword');
+img.addEventListener('click', function () {
+          img.setAttribute('src', '../img/senha aberta.png');
+});
