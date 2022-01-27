1ec0874770c816fe5ef1c7a3463ec5d2a4a71044
Vinnicius MOntanha <viniciusmassis01@hotmail.com>
2022-01-27 15:34:19 -0300

    fix eye password

--- goldensolutions/js/main.js
+++ goldensolutions/js/main.js
@@ -1,5 +1,11 @@
-var input = document.getElementById('#inputPassword');
-var img = document.getElementById('#togglePassword');
-img.addEventListener('click', function () {
-          img.setAttribute('src', '../img/senha aberta.png');
-});
+function showPassword() {
+          var a = document.getElementById("inputPassword");
+          var b = document.getElementById("toggle");
+          if (a.type == "password") {
+                    a.type = "text";
+                    b.src = "./img/senha aberta.png";
+          } else {
+                    a.type = "password";
+                    b.src = "./img/mostrat senha.png";
+          }
+}
